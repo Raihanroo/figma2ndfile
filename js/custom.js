bb5a4181a13080be00705041e7b715db2143eca8
Raihanroo <raihanroo21@gmail.com>
2022-10-31 23:13:26 +0300

    raihan

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+
+
+$(".cliend_slide").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow:'<i class="fa-solid fa-angle-right next"></i>',
+  prevArrow:'<i class="fa-solid fa-angle-left prev"></i>',
+  speed: 2000,
+  autoplay: true,
+ 
+})
+
+var mixer = mixitup('.mixitup_body');
